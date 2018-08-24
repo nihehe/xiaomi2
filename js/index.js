6dae61c45b6bbbe3dac8c8eee73b9daba2c7f849
nihehe <3154250143@qq.com>
2018-08-24 10:38:13 +0800

    lunbotu

--- js/index.js
+++ js/index.js
@@ -128,87 +128,96 @@ window.onload=function () {
         }
    }*/
     //平移效果轮播图
-    let banner_picture=document.querySelector(".banner-picture")
+    let banner_picture=document.querySelector(".banner")
+    let width=parseInt(getComputedStyle(banner_picture,null).width);
     let banner=document.querySelectorAll(".banner-picture li");
     let btnLeft=document.querySelector(".btnLeft");
     let btnRight=document.querySelector(".btnRight");
     let lis=document.querySelectorAll(".spot li");
     let now=next=0;
-    let t=setInterval(move,3000);
+    let flag=true;
+    let t=setInterval(move,2000);
     console.log(banner, lis);
-    banner_picture.onmouseenter=function () {
-        clearInterval(t);
-    }
-    banner_picture.onmouseleave=function () {
-        t=setInterval(move,3000);
-    }
+
     function move() {
         next++;
         if (next==banner.length){
             next=0;
         }
-        lis.forEach(function (v) {
-            v.className="";
-        })
-        banner[next].style.left="1226px";
-        banner[now].style.left="-1226px";
-        banner[next].style.left="0";
+        banner[next].style.left=width+"px";
+        animate (banner[now],{left:-width});
+        animate (banner[next],{left:0},function () {
+            flag=true;
+        });
+        lis[now].className="";
+        lis[next].className="click";
         now=next;
-        lis[now].className="click";
-
-
-        // animate (banner[now],{left:-1226},500);
-        // animate (banner[next],{left:0},500);
     }
     function moveL() {
         next--;
         if (next<0){
             next=banner.length-1;
         }
-        lis.forEach(function (v) {
-            v.className="";
-        })
-        banner[next].style.left="-1226px";
-        banner[now].style.left="1226px";
-        banner[next].style.left="0";
+        banner[next].style.left=-width+"px";
+        animate (banner[now],{left:width});
+        animate (banner[next],{left:0},function () {
+            flag=true;
+        });
+        lis[now].className="";
+        lis[next].className="click";
         now=next;
-        lis[now].className="click";
     }
     btnLeft.onclick=function () {
+        if (flag==false){
+            return;
+        }
+        if (next==0){
+            return;
+        }
+        flag=false;
         moveL();
     }
     btnRight.onclick=function () {
+        if (flag==false){
+            return;
+        }
+        if (next==banner.length-1){
+            next=banner.length-1;
+            return;
+        }
+        flag=false;
         move();
     }
+    banner_picture.onmouseenter=function () {
+        clearInterval(t);
+    }
+    banner_picture.onmouseleave=function () {
+        t=setInterval(move,2000);
+    }
+
     lis.forEach(function (v,i) {
-        lis[i].onclick=function () {
-            lis.forEach(function () {
-                lis[now].className="";
-            })
+        v.onclick=function () {
             if (i==now){
                 return;
             }else if(i<now){
-                banner[i].style.left="-1226px";
-                banner[now].style.left="1226px";
-                banner[i].style.left="0";
+                banner[i].style.left=-width+"px";
+                animate (banner[now],{left:width});
+                animate (banner[i],{left:0});
+                lis[now].classList.remove("click");
+                lis[i].classList.add("click");
             }
             else {
-                banner[i].style.left="1226px";
-                banner[now].style.left="-1226px";
-                banner[i].style.left="0";
+                banner[i].style.left=width+"px";
+                animate (banner[now],{left:-width});
+                animate (banner[i],{left:0});
+                lis[now].classList.remove("click");
+                lis[i].classList.add("click");
             }
-            lis[i].className="click";
             next=now=i;
         }
     })
 
-
-
-
-
-
-
-   //内容
+    //内容
 /*    let book1=document.getElementsByClassName("book1");
     let book=document.getElementsByClassName("book")[0];
     let spot2=book.getElementsByClassName("spot2 spot1")[0];
